daef065a363e243dfc41e5b47131f0de7e591f98
Kirill <kirill.uvarov@gmail.com>
2022-09-30 13:20:39 +0300

    34 done

--- Seminars/Seminar_5/Homework_S5/Zadacha_34/Program.cs
+++ Seminars/Seminar_5/Homework_S5/Zadacha_34/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа
+int[] array = new int [4];
+
+void FillArray(int[] collection) 
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(100, 1000);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    //Console.Write("A[");
+    while (position < count)
+    {
+        Console.Write($"{col[position]}, ");
+        position++;
+    }
+    //Console.Write("]");
+}
+
+FillArray(array);
+PrintArray(array);
+
+int evenNumber = 0;
+for (int i = 0; i  < array.Length; i++)
+{    
+    if(array[i] % 2 == 0)
+    {
+        evenNumber++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Количество чётных элементов: {evenNumber} ");
