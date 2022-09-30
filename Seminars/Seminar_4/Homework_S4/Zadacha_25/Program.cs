8151aea0445f5b0c6aa0f4db192a32a616fa2742
Kirill <kirill.uvarov@gmail.com>
2022-09-30 12:24:06 +0300

    Seminar_5.2

---
+++ Seminars/Seminar_4/Homework_S4/Zadacha_25/Program.cs
@@ -0,0 +1,14 @@
+﻿//Возведение заданного числа в заданную степень 
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень возведения: ");
+int value = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int result = 1;
+
+while (count < value)
+{
+    result = result * number;
+    count++;
+    }
+Console.WriteLine(result);
---
+++ Seminars/Seminar_4/Homework_S4/Zadacha_29/Program.cs
@@ -0,0 +1,11 @@
+﻿// Принимает 8 чисел, задаёт из них массив элементов и выводит на экран
+
+int[] array;
+int n = 8;
+array = new int [n];
+for (int i = 0; i < n; i++)
+{
+    Console.Write("A[{0}]= ", i);
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write($"A[{array[0]}, {array[1]}, {array[2]}, {array[3]}, {array[4]}, {array[5]}, {array[6]}, {array[7]}]");
