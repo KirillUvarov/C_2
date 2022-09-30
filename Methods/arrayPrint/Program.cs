4d5984e88ee113429456b563a348d8abe23e9ed1
Kirill <kirill.uvarov@gmail.com>
2022-09-30 12:51:31 +0300

    Создал папку с методами

---
+++ Methods/arrayPrint/Program.cs
@@ -0,0 +1,12 @@
+﻿//Метод вывода массива в терминал
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{col[position]} ");
+        position++;
+    }
+}
---
+++ Methods/fillArray/Program.cs
@@ -0,0 +1,11 @@
+﻿// Метод, который создаёт рандомный массив из чисел от -9 до 9 (10 не включается)
+void FillArray(int[] collection) // void - метод, который ничего не возвращает
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(-9,10);
+        index++;
+    }
+}
