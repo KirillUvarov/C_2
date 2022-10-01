f2499dbbc32e5408e3b01112460af79230cc140c
Kirill <kirill.uvarov@gmail.com>
2022-10-01 13:52:15 +0300

    Создал папку с методами4

---
+++ Methods/PrintArrayMatrix/Program.cs
@@ -0,0 +1,15 @@
+﻿int[,] matrix = new int[3, 4];
+
+void PrintArrayMatrix(int[,] matr) // Метод вывода на консоль матрицы двумерного массива
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for(int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
+}
+
+PrintArrayMatrix(matrix);
