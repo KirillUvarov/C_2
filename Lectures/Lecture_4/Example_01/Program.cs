2de7d74758de064928486f413cb2b6a9bde83943
Kirill <kirill.uvarov@gmail.com>
2022-10-01 13:49:55 +0300

    лекция 4.3

--- Lectures/Lecture_4/Example_01/Program.cs
+++ Lectures/Lecture_4/Example_01/Program.cs
@@ -1,3 +1,48 @@
-﻿    
-int[,] matrix = new int[5, 8];
-Console.WriteLine(matrix);
+﻿// string[,] table = new string[2, 5];
+// // table[0,0]   table[0,1] table[0,2]   table[0,4]
+// // table[0,1]
+
+
+// table[1, 2] = "слово";
+
+// for(int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         Console.WriteLine($"-{table[rows, columns]}-");
+//     }
+// }
+
+
+int[,] matrix = new int[3, 4];
+
+void PrintArrayMatrix(int[,] matr) // Метод вывода на консоль матрицы двумерного массива
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for(int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
+}
+
+
+void FillArrayMatrix(int[,] matr) // void - метод, который ничего не возвращает
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(0); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10); //[1, 10)
+        }
+    }
+}
+
+
+
+PrintArrayMatrix(matrix);
+FillArrayMatrix(matrix);
+Console.WriteLine();
+PrintArrayMatrix(matrix);
--- Lectures/Lecture_4/Example_03/Program.cs
+++ Lectures/Lecture_4/Example_03/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Факториал
+
+double Factorial(int n) // int заменили на double
+{
+    // 1! = 1
+    // 0! = 1
+    if (n == 1) return 1;
+    else return n * Factorial(n-1);
+
+}
+
+// Console.WriteLine(Factorial(3)); // 3! = 1 * 2 * 3 = 6
+// Console.WriteLine(Factorial(20)); // 20! = -2102132736
+// Console.WriteLine(); // Пустая строка
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
