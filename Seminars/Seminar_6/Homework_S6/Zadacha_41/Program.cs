5fbd6682bac190c9d98bf4f802e9357775201a57
Kirill <kirill.uvarov@gmail.com>
2022-10-04 14:55:55 +0300

    Сделал задачу 41

--- Seminars/Seminar_6/Homework_S6/Zadacha_41/Program.cs
+++ Seminars/Seminar_6/Homework_S6/Zadacha_41/Program.cs
@@ -2,3 +2,36 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
+Console.WriteLine("Введите цифру, соответствующую количеству чисел, которые собераетесь ввести на следующем этапе");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Пошагово введите {m} положительных или отрицательных чисел: ");
+
+int[] array = new int [m];
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array [i] = Convert.ToInt32(Console.ReadLine());
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    Console.Write("Ваш массив: A[");
+    while (position < count -1)
+    {
+        Console.Write($"{col[position]}, ");
+        position++;
+               
+    }
+    Console.Write($"{col[position]}] ");
+}
+
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($"Вы ввели {m} чисел, среди которых {count} положительных");
--- Seminars/Seminar_6/Homework_S6/Zadacha_43/Program.cs
+++ Seminars/Seminar_6/Homework_S6/Zadacha_43/Program.cs
@@ -1,3 +1,28 @@
 ﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
-//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//заданных уравнениями 
+//y = k1 * x + b1, 
+//y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите координату X первой точки ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Y первой точки ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату Z второй точки ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату X второй точки ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите координату Y второй точки ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату Z второй точки ");
+// int z2 = Convert.ToInt32(Console.ReadLine());
+
+double y = 0;
+double x = 0;
+y = k1 * x + b1;
+y = k2 * x + b2; // Math.Sqrt - извлечение корня
+y = Math.Round(d,2); // округление до 2х символов после запятой
+Console.WriteLine(d);
+//Math.Pow(d,2); - возведение в степень
