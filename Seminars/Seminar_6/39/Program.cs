63cdf4f51570f10973518d9400caaf8118a91829
Kirill <kirill.uvarov@gmail.com>
2022-10-05 15:25:35 +0300

    Сделал задачу 39

--- Seminars/Seminar_6/39/Program.cs
+++ Seminars/Seminar_6/39/Program.cs
@@ -4,39 +4,39 @@
 
 // [6 7 3 6] -> [6 3 7 6]
 
-// void FAR(int[] mas,int z, int f)
-// {
-//     int index=0;
-//     while (index<mas.Length)
-//     {
-//         mas[index] = new Random().Next(z, f);
-//         index++;
-//     }
-// }
-void Print(int[]mas)
+void FAR(int[] mas, int z, int f)
 {
-    int i=0;
-    while (i<mas.Length)
+    int index = 0;
+    while (index < mas.Length)
+    {
+        mas[index] = new Random().Next(z, f);
+        index++;
+    }
+}
+void Print(int[] mas)
+{
+    int i = 0;
+    while (i < mas.Length)
     {
         Console.Write($"{mas[i]} ");
         i++;
     }
 }
-// Console.Write("Введите размер массива - ");
-// int a = Convert.ToInt32(Console.ReadLine());
-// int [] arr= new int[a];
-// FAR(arr,100,1000);
-// Print(arr);
-// int help=0;
-// int i=0;
-// int c=1;
-// while (i<arr.Length/2)
-// {
-//     help=arr[i];
-//     arr[i]=arr[arr.Length-c];
-//     arr[arr.Length-c]=help;
-//     i++;
-//     c++;
-// }
-// Console.WriteLine(" ");
-// Print(arr);
+Console.Write("Введите размер массива - ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[a];
+FAR(arr, 0, 10);
+Print(arr);
+int help = 0;
+int i = 0;
+int c = 1;
+while (i < arr.Length / 2)
+{
+    help = arr[i];
+    arr[i] = arr[arr.Length - c];
+    arr[arr.Length - c] = help;
+    i++;
+    c++;
+}
+Console.WriteLine(" ");
+Print(arr);
