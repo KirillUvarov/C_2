2ec6c0daaa42a2bf42aeac5440d78810abdb5d4e
Kirill <kirill.uvarov@gmail.com>
2022-10-08 13:13:58 +0300

    Лекция 7

---
+++ Lectures/Lecture_6/Example_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// using System.Linq;
+
+// string text = "(1,2) (2,3) (4,5) (6,7)";
+//             .Replace("(", "")
+
+// var data = text.Split("")
+//             .ToArray
+// Console.WriteLine(text);
+
+// ///
